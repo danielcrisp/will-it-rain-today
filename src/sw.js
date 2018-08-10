9551801c7b76359f976ac1a288f716b29c1f629d
danielcrisp <daniel@danielcrisp.com>
2018-08-10 18:51:53 +0100

    Filter unwanted events

--- src/sw.js
+++ src/sw.js
@@ -8,5 +8,31 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+
+  // Ignore crossdomain requests
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+
+  // Ignore non-GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  // Ignore browser-sync
+  if (event.request.url.indexOf('browser-sync') > -1) {
+    return;
+  }
+
+  // Prevent index route being cached
+  if (event.request.url === (self.location.origin + '/')) {
+    return;
+  }
+
+  // Prevent index.html being cached
+  if (event.request.url.endsWith('index.html')) {
+    return;
+  }
   console.log(event);
+
 });
